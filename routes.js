057ee83120a0bcbc8b918a6b7bd2f269e8cd0bdc
willkrolik <38799464+willkrolik@users.noreply.github.com>
2019-11-24 12:30:50 -0800

    looks to be completed, not fully sure about users

--- routes.js
+++ routes.js
@@ -5,14 +5,14 @@ const { Course, User } = require('./models');
 const bcrypt = require('bcryptjs');
 
 
-function asyncHandler(cb){
-    return async (req,res, next) => {
-        try {
-            await cb(req, res, next);
-        } catch(err) {
-            next(err);
-        }
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
     }
+  }
 }
 
 /**
@@ -24,11 +24,10 @@ function asyncHandler(cb){
 const authenticateUser = (req, res, next) => {
   let message = null;
 
-  // Get the user's credentials from the Authorization header.
-  const credentials = { name: 'admin@project9.org', pass: 'password' };//auth(req);
+ 
+  const credentials = { name: 'admin@project9.org', pass: 'password' };;
 
   if (credentials) {
-    // Look for a user whose `username` matches the credentials `name` property.
     User.findOne({
       where: {
         emailAddress: credentials.name
@@ -57,133 +56,120 @@ const authenticateUser = (req, res, next) => {
       }
     });
   }
-  
+
 };
 
-// Construct a router instance.
 
-
-// Return a list of all courses method
-router.get('/courses', asyncHandler(async(req, res, next) => {
-    Course.findAll({
-        order: [["id", "ASC"]],
-        attributes: ['id','userId', 'title', 'description', 'estimatedTime', 'materialsNeeded', ]
-    })
+// Return a list of all courses
+router.get('/courses', asyncHandler(async (req, res, next) => {
+  Course.findAll({
+    order: [["id", "ASC"]],
+    attributes: ['id', 'userId', 'title', 'description', 'estimatedTime', 'materialsNeeded',]
+  })
     .then(courses => {
-        res.json({ courses });
+      res.json({ courses });
     })
-  }))
+}))
 
-// get route for specific ID of course
-  router.get('/courses/:id', asyncHandler(async(req, res, next) => {
-    Course.findOne({
-      attributes: ['id', 'userId', 'title', 'description', 'estimatedTime', 'materialsNeeded'],
-      where: {
-        id: req.params.id
-      }
-    }).then(course => {
-      if(course) {
-        res.json({ course })
-      } else {
-        res.status(404).json({ message: 'Route not found' });
-      }
-    })
-  }))
+// get individual course
+router.get('/courses/:id', asyncHandler(async (req, res, next) => {
+  Course.findOne({
+    attributes: ['id', 'userId', 'title', 'description', 'estimatedTime', 'materialsNeeded'],
+    where: {
+      id: req.params.id
+    }
+  }).then(course => {
+    if (course) {
+      res.json({ course })
+    } else {
+      res.status(404).json({ message: 'Route not found' });
+    }
+  })
+}))
 
-// router post
+// creates course and description, original route code credit to ISimpson
 router.post('/courses', [
-    check('title').exists().withMessage('Please provide a value for title'),
-    check('description').exists().withMessage('Please provide a value for description')
-  ], authenticateUser, asyncHandler(async(req, res, next)=> {
-    const user = req.currentUser.id;
+  check('title').exists().withMessage('Value required for title'),
+  check('description').exists().withMessage('Value required for description')
+], authenticateUser, asyncHandler(async (req, res, next) => {
+  const user = req.currentUser.id;
 
-    console.log('course request', req.body);
-    // validation result
-    const errors = validationResult(req);
+  console.log('course request', req.body);
+  const errors = validationResult(req);
 
-     // validation errors
-     if (!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(error => error.msg);
+    res.status(400).json({ errors: errorMessages });
+  } else {
 
-         // list of error messages
-         const errorMessages = errors.array().map(error => error.msg);
-
-          // validation client side
-         res.status(400).json({ errors: errorMessages });
-     } else {
-
-        // new course
-        await Course.create({ ...req.body, userId: user })
-        .then((course) => {
-            if (course) {
-                res.status(201).location(`/api/courses/${course.id}`).end();
-            } else {
-                next();
-            }
-        })
-     }
-  }))
-
-  router.put('/courses/:id', [
-    check('title').exists().withMessage('Please provide a value for title'),
-    check('description').exists().withMessage('Please provide a value for description')
-  ], authenticateUser, asyncHandler(async(req, res, next) => {
-    const user = req.currentUser.id;
-
-
-    const errors = validationResult(req);
-
-     // errors
-     if (!errors.isEmpty()) {
-
-         // map() goes through the list in the array a prints out message
-         const errorMessages = errors.array().map(error => error.msg);
-
-         // validation returned / error 404
-         res.status(400).json({ errors: errorMessages });
+    
+    await Course.create({ ...req.body, userId: user })
+      .then((course) => {
+        if (course) {
+          res.status(201).location(`/api/courses/${course.id}`).end();
         } else {
-            await Course.findOne({
-              where: [{ id: req.params.id }]
-            })
-            .then((course) => {
-
-                // if you select  a user to update
-                if (course.userId === user) {
-                    if (course) {
-                        course.update(req.body);
-                        res.status(204).end();
-                    } else {
-                      next();
-                    }
-                } else {
-                  res.status(403).json({ message: "Current User doesn't own the requested course" }).end();
-                }
-            })
+          next();
         }
-    }))
+      })
+  }
+}))
+//modify existing course, original code credit to ISimpson
+router.put('/courses/:id', [
+  check('title').exists().withMessage('Value required for for title'),
+  check('description').exists().withMessage('Value required for description')
+], authenticateUser, asyncHandler(async (req, res, next) => {
+  const user = req.currentUser.id;
 
-  // router delete
-  router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res, next)=> {
-    const user = req.currentUser.id;
 
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+
+    const errorMessages = errors.array().map(error => error.msg);
+
+
+    res.status(400).json({ errors: errorMessages });
+  } else {
     await Course.findOne({
       where: [{ id: req.params.id }]
     })
-    .then((course) => {
-         // If user has the selected course delete it
-            if (course.userId === user) {
-              if (course) {
-                course.destroy();
-                res.status(204).end();
-            } else {
-              next();
-            }
-         } else {
+      .then((course) => {
+        if (course.userId === user) {
+          if (course) {
+            course.update(req.body);
+            res.status(204).end();
+          } else {
+            next();
+          }
+        } else {
           res.status(403).json({ message: "Current User doesn't own the requested course" }).end();
-         }
+        }
       })
-  }))
+  }
+}))
 
-// Route that returns the current authenticated user.
+// deletes specifc course, original code credit to ISimpson
+router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res, next) => {
+  const user = req.currentUser.id;
+
+  await Course.findOne({
+    where: [{ id: req.params.id }]
+  })
+    .then((course) => {
+      if (course.userId === user) {
+        if (course) {
+          course.destroy();
+          res.status(204).end();
+        } else {
+          next();
+        }
+      } else {
+        res.status(403).json({ message: "Current User doesn't own the requested course" }).end();
+      }
+    })
+}))
+
+// returns current user 
 router.get('/users', authenticateUser, (req, res) => {
   const user = req.currentUser;
 
@@ -197,41 +183,38 @@ router.get('/users', authenticateUser, (req, res) => {
 router.post('/users', [
   check('name')
     .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "name"'),
+    .withMessage('Value required for "name"'),
   check('username')
     .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "username"'),
+    .withMessage('Value required for "username"'),
   check('password')
     .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "password"'),
+    .withMessage('Value required for for "password"'),
 ], (req, res) => {
-  // Attempt to get the validation result from the Request object.
   const errors = validationResult(req);
 
-  // If there are validation errors...
   if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
     const errorMessages = errors.array().map(error => error.msg);
 
-    // Return the validation errors to the client.
+    
     return res.status(400).json({ errors: errorMessages });
   }
 
-  // Get the user from the request body.
+ 
   const user = req.body;
 
-  // Hash the new user's password.
+  
   user.password = bcrypt.hashSync(user.password);
 
-  // Saved in the database
+  
   Users.create(user).then((user, created) => {
-    // Set the status to 201 Created and end the response.
+    
     return res.status(201).end();
   }).catch(err => {
     return res.status(422).end();
   });
 
-  
+
 });
 
 module.exports = router;
