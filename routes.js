f88d6bb2ff80c4526e2664f5ca14c5f07ea05fc4
William Krolik <wkrolik@InComm.com>
2019-11-27 14:04:11 -0800

    trying to understand GET users route

--- routes.js
+++ routes.js
@@ -36,28 +36,17 @@ const authenticateUser = (req, res, next) => {
   // Parse the user's credentials from the Authorization header.
   const credentials = auth(req);
 
-  // If the user's credentials are available...
   if (credentials) {
-    // Attempt to retrieve the user from the data store
-    // by their username (i.e. the user's "key"
-    // from the Authorization header).
+    // Look for a user whose `username` matches the credentials `name` property.
     const user = users.find(u => u.username === credentials.name);
 
-    // If a user was successfully retrieved from the data store...
     if (user) {
-      // Use the bcryptjs npm package to compare the user's password
-      // (from the Authorization header) to the user's password
-      // that was retrieved from the data store.
       const authenticated = bcryptjs
         .compareSync(credentials.pass, user.password);
-
-      // If the passwords match...
       if (authenticated) {
         console.log(`Authentication successful for username: ${user.username}`);
 
-        // Then store the retrieved user object on the request object
-        // so any middleware functions that follow this middleware function
-        // will have access to the user's information.
+        // Store the user on the Request object.
         req.currentUser = user;
       } else {
         message = `Authentication failure for username: ${user.username}`;
@@ -69,19 +58,13 @@ const authenticateUser = (req, res, next) => {
     message = 'Auth header not found';
   }
 
-  // If user authentication failed...
   if (message) {
     console.warn(message);
-
-    // Return a response with a 401 Unauthorized HTTP status code.
     res.status(401).json({ message: 'Access Denied' });
   } else {
-    // Or if user authentication succeeded...
-    // Call the next() method.
     next();
   }
 };
-
 
 // Return a list of all courses
 router.get('/courses', asyncHandler(async (req, res, next) => {
